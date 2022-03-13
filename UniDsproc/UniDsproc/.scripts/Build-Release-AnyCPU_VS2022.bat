b582723a8d4e925994d5a3d3820e241324381cd3
Tony Skorik <just.skorik@gmail.com>
2022-03-13 16:37:06 +0400

    Add script for 2022 MsBuild build;

---
+++ UniDsproc/UniDsproc/.scripts/Build-Release-AnyCPU_VS2022.bat
@@ -0,0 +1,10 @@
+cls
+
+pushd ..\..\..\
+git pull
+
+::pause
+
+popd
+
+"c:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\MSBuild.exe" /property:Configuration=Release;Platform=AnyCPU ..\UniDsproc.csproj
