585151361c736e93f46658710c7da16f1432d88b
major <gleb@crc.ru>
2022-04-13 20:41:04 +0300

    build bat-file for Gleb

--- UniDsproc/UniDsproc/.scripts/Build-Release-AnyCPU_VS2022.bat
+++ UniDsproc/UniDsproc/.scripts/Build-Release-AnyCPU_VS2022.bat
@@ -8,3 +8,5 @@ git pull
 popd
 
 "c:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\MSBuild.exe" /property:Configuration=Release;Platform=AnyCPU ..\UniDsproc.csproj
+
+pause
