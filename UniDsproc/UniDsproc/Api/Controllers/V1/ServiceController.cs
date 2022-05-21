d4050bd9dcbbdb571af0fe5c89325c42860972bc
Tony Skorik <just.skorik@gmail.com>
2022-05-21 17:49:19 +0400

    Add version controller

---
+++ UniDsproc/UniDsproc/Api/Controllers/V1/ServiceController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using UniDsproc.Configuration;
+
+namespace UniDsproc.Api.Controllers.V1
+{
+	[RoutePrefix("api/v1/service")]
+	public class ServiceController : ApiController
+	{
+		private readonly AppSettings _settings;
+
+		public ServiceController(AppSettings settings)
+		{
+			_settings = settings;
+		}
+
+		[HttpGet]
+		[Route("version")]
+		public IHttpActionResult Version()
+		{
+			return Ok(Program.Version);
+		}
+	}
+}
